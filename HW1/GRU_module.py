323cae986ae2edd1353fbf89fd35f8a8d9593db5
YH.Wu <r05548020@ntu.edu.tw>
2017-03-07 23:59:45 +0800

    GRU v1.0.2

--- HW1/GRU_module.py
+++ HW1/GRU_module.py
@@ -18,17 +18,20 @@ def BiGRU(inputs,num_layers,num_hidden_neurons,output_size,dropout,activation,na
 	Multi_bGRUcell_withDropout = tf.nn.rnn_cell.MultiRNNCell([bGRUcell_withDropout] * num_layers)
 
 
-	output_h, state_c = tf.nn.bidirectional_dynamic_rnn(
+	_output_h, _state_c = tf.nn.bidirectional_dynamic_rnn(
 		                                				cell_fw=Multi_fGRUcell_withDropout,
 		                                				cell_bw=Multi_bGRUcell_withDropout,
 		                                				dtype=tf.float32,
 		                                				sequence_length=sequence_length,
 		                                				inputs=inputs
-		                                				) 
+		                                				) ##(2,batch,max_steps,num_hiden_neurons), (2,layer,batch,num_hidden_neurons)
 
 
-	output = tf.concat(2, output_h)
-	output_h = tf.reshape(output_h, [-1, 2*num_hidden_neurons])
+	output_h = tf.concat(2, _output_h) ##(batch,max_steps,2*num_hiden_neurons)
+	output_h = tf.reshape(output_h, [-1, 2*num_hidden_neurons]) ##(batch*max_steps,2*num_hiden_neurons)
+
+
+
 
 	with tf.variable_scope('GRU_'+name):
 		weights = tf.get_variable("weights", [2*num_hidden_neurons,output_size],initializer=tf.truncated_normal_initializer(stddev=0.1,dtype=tf.float32))
@@ -44,5 +47,5 @@ def BiGRU(inputs,num_layers,num_hidden_neurons,output_size,dropout,activation,na
 	else:
 		output = tf.nn.relu(tf.matmul(output_h,weights)+bias)
 
-	output = tf.reshape(output,[inputs_shape[0],inputs_shape[1],output_size])
+	output = tf.reshape(output,[inputs_shape[0],inputs_shape[1],output_size]) ##(batch,max_steps,output_size)
 	return output
--- HW1/test_GRU_module.py
+++ HW1/test_GRU_module.py
@@ -8,17 +8,24 @@ input_shape = [50,250,300]
 a=np.random.randn(input_shape[0],input_shape[1],input_shape[2])
 x = tf.placeholder(shape=(input_shape[0],input_shape[1],input_shape[2]),dtype=tf.float32)
 gru_output = GRU.BiGRU(	inputs=x,
-						num_layers=2,
+						num_layers=1,
 						num_hidden_neurons=128,
 						output_size=200,
 						dropout=0.5,
 						activation='relu',
 						name='test',
 						sequence_length=None
-					)
-gru_output_shape_use = gru_output.get_shape()
-gru_output_shape_print = tf.shape(gru_output)
+					) #(batch,max_steps,output_size)
+gru_output_shape_use = gru_output[0].get_shape()
+gru_output_shape_print1 = tf.shape(gru_output)
+
+
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
-print(sess.run(gru_output_shape_print,feed_dict={x:a}))
+print(sess.run([gru_output_shape_print1],feed_dict={x:a}))
+
+###HOW TO GET LAST　OUTPUT###
+gru_output = tf.transpose(gru_output, [1, 0, 2]) ##(max_steps,batch,output_size)
+last = tf.gather(gru_output, int(gru_output.get_shape()[0]) - 1) ##(batch,output_size)
+print(sess.run(tf.shape(last),feed_dict={x:a}))
