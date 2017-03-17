f01e7100c566103c54b6766579c6db64827c9723
RasonLiu <davidliu4417@gmail.com>
2017-03-17 19:39:07 +0800

    runnable, with 2 model, normal and nce_loss

--- hw1/input.py
+++ hw1/input.py
@@ -9,10 +9,11 @@ class DataLoader():
         self.vocab_size = vocab_size
         
         data_len = len(input_words)
-        self.sentence_count = data_len // (num_steps + 1)
+        self.sentence_count = data_len // (num_steps)
         
-        self.sentence_arr = np.array(input_words[:self.sentence_count * (num_steps+1)]).reshape((self.sentence_count, num_steps+1))
-    
+        self.sentence_arr = np.array(input_words[:self.sentence_count * (num_steps)]).reshape((self.sentence_count, num_steps))
+        
+        
     def shuffle(self):
         np.random.shuffle(self.sentence_arr)
     
@@ -25,7 +26,7 @@ class DataLoader():
         def idx2vec(num_steps, vocab_size, q):
             
             for i in range(self.sentence_count):
-                vec = np.zeros([num_steps + 1, vocab_size])
+                vec = np.zeros([num_steps, vocab_size])
                 
                 sentence = self.sentence_arr[i]
                 for j, one_hot_idx in enumerate(sentence):
@@ -42,7 +43,7 @@ class DataLoader():
         p.daemon = True
         p.start()
         
-        x = np.zeros((batch_size, self.num_steps + 1, self.vocab_size))
+        x = np.zeros((batch_size, self.num_steps, self.vocab_size))
         
         for i in range(0, self.sentence_count, batch_size):
             for j in range(batch_size):
@@ -56,4 +57,62 @@ class DataLoader():
                 
             yield x
        
+class DataLoader2():
+    def __init__(self, input_words, num_steps=40, vocab_size=30000):
+        self.num_steps = num_steps
+        self.vocab_size = vocab_size
+        
+        data_len = len(input_words)
+        self.sentence_count = data_len // (num_steps)
+        
+        self.sentence_arr = np.array(input_words[:self.sentence_count * (num_steps)]).reshape((self.sentence_count, num_steps))
+        
+        
+    def shuffle(self):
+        np.random.shuffle(self.sentence_arr)
+    
+    def batch_count(self, batch_size):
+        return (self.sentence_arr.shape[0] // batch_size) + 1
+    
+    def batch_gen(self, batch_size):
+        QUEUE_END = '__QUEUE_END105834569xx'
+        
+        def idx2vec(num_steps, vocab_size, batch_size, q):            
+            batch_count_epoch = self.sentence_count // batch_size + 1
+      
+            for i in range(batch_count_epoch):
+                x = np.zeros([batch_size, num_steps, vocab_size])
+                for j in range(batch_size):
+                    sentence_idx = i*batch_size + j
+                    
+                    if sentence_idx < self.sentence_count:
+                        sentence = self.sentence_arr[sentence_idx]
+                        for k, one_hot_idx in enumerate(sentence):
+                            x[j, k, one_hot_idx] = 1
+                    else: 
+                        x = np.delete(x, range(j, batch_size), axis=0)
+                        break
+                        
+                q.put(x)
+                    
+            q.put(QUEUE_END)
+            q.close()
+            
+        q = mp.Queue(maxsize=20)
+        
+        p = mp.Process(target=idx2vec, args=(self.num_steps, self.vocab_size, batch_size, q))
+        p.daemon = True
+        p.start()
+        
+        x = np.zeros((batch_size, self.num_steps, self.vocab_size))
+        
+        
+        batch_count_epoch = self.sentence_count // batch_size + 1
+        for i in range(batch_count_epoch):
+            x = q.get()
+                
+            if x == QUEUE_END:  
+                break
+                
+            yield x
         
--- hw1/rason_model.py
+++ hw1/rason_model.py
@@ -1,66 +1,41 @@
 import tensorflow as tf
 
 class biGRU_model(object):
-
-    def __init__(self, batch_size=20, num_steps=40, vocab_size=30000, num_hidden=800, dropout_rate=0.5, num_layers=2,):
+    def __init__(self, num_steps=40, vocab_size=30000, num_hidden=800, dropout_rate=0.5, num_layers=2):
         
         ### input.shape = (batch_size, num_steps+1, vocab_size)
-        self.sentences = tf.placeholder(shape=(None, num_steps + 1, vocab_size), dtype=tf.float32)
+        self.sentences = tf.placeholder(shape=(None, num_steps, vocab_size), dtype=tf.float32)
         
-        fw_input = self.sentences[:,:num_steps, :]
-        bw_input = tf.reverse(self.sentences[:,1:,:], [1])
         final_output = self.sentences[:,1:-1,:]
         
-        with tf.variable_scope('fwGRU'):
-            with tf.device('/cpu:0'):
-                fwGRU_cell = tf.contrib.rnn.GRUCell(num_hidden)
-                fwGRU_withDropout = tf.contrib.rnn.DropoutWrapper(fwGRU_cell, output_keep_prob=dropout_rate)
-                fw_multiGRU_cell = tf.contrib.rnn.MultiRNNCell([fwGRU_withDropout] * num_layers)
+        fwGRU_cell = tf.contrib.rnn.GRUCell(num_hidden)
+        fwGRU_withDropout = tf.contrib.rnn.DropoutWrapper(fwGRU_cell, output_keep_prob=dropout_rate)
+        fw_multiGRU_cell = tf.contrib.rnn.MultiRNNCell([fwGRU_withDropout] * num_layers)
         
-        with tf.variable_scope('bwGRU'):
-            with tf.device('/cpu:0'):
-                bwGRU_cell = tf.contrib.rnn.GRUCell(num_hidden)
-                bwGRU_withDropout = tf.contrib.rnn.DropoutWrapper(bwGRU_cell, output_keep_prob=dropout_rate)
-                bw_multiGRU_cell = tf.contrib.rnn.MultiRNNCell([bwGRU_withDropout] * num_layers)
+        bwGRU_cell = tf.contrib.rnn.GRUCell(num_hidden)
+        bwGRU_withDropout = tf.contrib.rnn.DropoutWrapper(bwGRU_cell, output_keep_prob=dropout_rate)
+        bw_multiGRU_cell = tf.contrib.rnn.MultiRNNCell([bwGRU_withDropout] * num_layers)
         
-        self._fw_initial_state = fw_multiGRU_cell.zero_state(batch_size, tf.float32)
-        self._bw_initial_state = bw_multiGRU_cell.zero_state(batch_size, tf.float32)
+        batch_size = tf.shape(self.sentences)[0]
+        sentence_len = tf.fill([batch_size], num_steps)
         
-        fw_outputs = []
-        bw_outputs = []
+        outputs, states  = tf.nn.bidirectional_dynamic_rnn(
+                                cell_fw=fwGRU_cell,
+                                cell_bw=bwGRU_cell,
+                                dtype=tf.float32,
+                                sequence_length = sentence_len,
+                                inputs=self.sentences)  
         
-        fw_state = self._fw_initial_state
-        bw_state = self._bw_initial_state
-
-        with tf.variable_scope('fwGRU'):
-            for time_step in range(num_steps):
-                if time_step > 0: 
-                    tf.get_variable_scope().reuse_variables()
-                
-                fw_cell_output, fw_state = fw_multiGRU_cell(fw_input[:, time_step, :], fw_state)
-                fw_outputs.append(fw_cell_output)
-                
-        with tf.variable_scope('bwGRU'):
-            for time_step in range(num_steps):
-                if time_step > 0: 
-                    tf.get_variable_scope().reuse_variables()        
-                
-                bw_cell_output, bw_state = bw_multiGRU_cell(bw_input[:, time_step, :], bw_state)
-                bw_outputs.append(bw_cell_output)
-        
+        fw_output, bw_output = outputs
         
         ### fw_output.shape = (batch_size, num_steps, num_hidden) and means the 2nd~(num_steps+1)th word
-        ### bw_output.shape = (batch_size, num_steps, num_hidden) and means the (num_steps)th~1st word
-        ### consider only 2nd~(num_steps)th word in fw_output
-        ### consider only (num_steps)th~2nd word in bw_output
-        fw_output = tf.reshape(tf.concat(fw_outputs, 1), [batch_size, num_steps, num_hidden])
-        bw_output = tf.reshape(tf.concat(bw_outputs, 1), [batch_size, num_steps, num_hidden])
-        fw_output = fw_output[:,:-1,:]
-        bw_output = bw_output[:,:-1,:]
-        reverse_bw_output = tf.reverse(bw_output, [1])
+        ### bw_output.shape = (batch_size, num_steps, num_hidden) and means the 0th~(num_steps-1)th word
+        ### consider only 2nd~(num_steps-1)th word
+        fw_output = fw_output[:,:-2,:]
+        bw_output = bw_output[:,2:,:]
         
         ### [batch_size * (num_steps-1), 2*num_hidden]
-        gru_output_feature = tf.reshape(tf.concat([fw_output, reverse_bw_output], 2), [-1, 2*num_hidden])
+        gru_output_feature = tf.reshape(tf.concat([fw_output, bw_output], 2), [-1, 2*num_hidden])
         
         with tf.variable_scope('biGRU'):
             
@@ -68,7 +43,7 @@ class biGRU_model(object):
             ## DNN parameter
                 w_1 = tf.get_variable("w_dnn_1", [2*num_hidden, 6*num_hidden], initializer= tf.contrib.layers.xavier_initializer(dtype=tf.float32))
                 b_1 = tf.get_variable("b_dnn_1", [6*num_hidden], initializer= tf.contrib.layers.xavier_initializer(dtype=tf.float32))
-
+    
                 w_2 = tf.get_variable("w_dnn_2", [6*num_hidden, vocab_size], initializer= tf.contrib.layers.xavier_initializer(dtype=tf.float32))
                 b_2 = tf.get_variable("b_dnn_2", [vocab_size], initializer= tf.contrib.layers.xavier_initializer(dtype=tf.float32))
 
@@ -84,10 +59,12 @@ class biGRU_model(object):
         self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction,labels=label))
 
      	### [batch_size, num_steps-1, vocab_size]
-        self.predict_result = tf.reshape(prediction, [batch_size, num_steps-1, vocab_size] )
+        self.predict_result = tf.reshape(prediction, [batch_size, num_steps-2, vocab_size] )
         self.global_step = tf.Variable(0, trainable=False)
         self.train_op = tf.train.AdamOptimizer().minimize(self.cost, global_step=self.global_step)
-        self.sess = tf.Session(config=tf.ConfigProto(allow_soft_placement = True))
+        config = tf.ConfigProto(log_device_placement = True)
+        config.gpu_options.allow_growth = True
+        self.sess = tf.Session(config=config)
 
     def train(self,input_sentences):
         _,loss = self.sess.run([self.train_op,self.cost],feed_dict={self.sentences:input_sentences})
@@ -96,6 +73,85 @@ class biGRU_model(object):
     def predict(self,input_sentences):
         return self.sess.run(self.predict_result,feed_dict={self.sentences:input_sentences})
     
+    def initialize(self):
+        self.sess.run(tf.global_variables_initializer()) 
+
+        
+class biGRU_nce_model(object):
+    def __init__(self, num_steps=40, vocab_size=30000, num_hidden=800, dropout_rate=0.5, num_layers=2):
+        
+        ### input.shape = (batch_size, num_steps+1, vocab_size)
+        self.sentences = tf.placeholder(shape=(None, num_steps, vocab_size), dtype=tf.float32)
+        self.output_widx = tf.arg_max(self.sentences[:,1:-1,:], 2)
+        
+        fwGRU_cell = tf.contrib.rnn.GRUCell(num_hidden)
+        fwGRU_withDropout = tf.contrib.rnn.DropoutWrapper(fwGRU_cell, output_keep_prob=dropout_rate)
+        fw_multiGRU_cell = tf.contrib.rnn.MultiRNNCell([fwGRU_withDropout] * num_layers)
+        
+        bwGRU_cell = tf.contrib.rnn.GRUCell(num_hidden)
+        bwGRU_withDropout = tf.contrib.rnn.DropoutWrapper(bwGRU_cell, output_keep_prob=dropout_rate)
+        bw_multiGRU_cell = tf.contrib.rnn.MultiRNNCell([bwGRU_withDropout] * num_layers)
+        
+        batch_size = tf.shape(self.sentences)[0]
+        sentence_len = tf.fill([batch_size], num_steps)
+        
+        outputs, states  = tf.nn.bidirectional_dynamic_rnn(
+                                cell_fw=fwGRU_cell,
+                                cell_bw=bwGRU_cell,
+                                dtype=tf.float32,
+                                sequence_length = sentence_len,
+                                inputs=self.sentences)  
+        
+        fw_output, bw_output = outputs
+        
+        ### fw_output.shape = (batch_size, num_steps, num_hidden) and means the 2nd~(num_steps+1)th word
+        ### bw_output.shape = (batch_size, num_steps, num_hidden) and means the 0th~(num_steps-1)th word
+        ### consider only 2nd~(num_steps-1)th word
+        fw_output = fw_output[:,:-2,:]
+        bw_output = bw_output[:,2:,:]
+        
+        ### [batch_size * (num_steps-1), 2*num_hidden]
+        gru_output_feature = tf.reshape(tf.concat([fw_output, bw_output], 2), [-1, 2*num_hidden])
+        
+        with tf.variable_scope('biGRU'):
+            
+            with tf.device('/cpu:0'):
+            ## DNN parameter
+                w_1 = tf.get_variable("w_dnn_1", [2*num_hidden, 6*num_hidden], initializer= tf.contrib.layers.xavier_initializer(dtype=tf.float32))
+                b_1 = tf.get_variable("b_dnn_1", [6*num_hidden], initializer= tf.contrib.layers.xavier_initializer(dtype=tf.float32))
+    
+                w_2 = tf.get_variable("w_dnn_2", [vocab_size, 6*num_hidden], initializer= tf.contrib.layers.xavier_initializer(dtype=tf.float32))
+                b_2 = tf.get_variable("b_dnn_2", [vocab_size], initializer= tf.contrib.layers.xavier_initializer(dtype=tf.float32))
+
+     	### [batch_size * (num_steps-1), 6*width]
+        dnn_output_1 = tf.nn.relu(tf.matmul(gru_output_feature, w_1) + b_1)
+        prediction = tf.matmul(dnn_output_1, tf.transpose(w_2)) + b_2
+        
+        out_widx = tf.reshape(self.output_widx, [-1,1])
+        
+        self.cost = tf.reduce_mean(
+              tf.nn.nce_loss(weights=w_2,
+                             biases=b_2,
+                             labels=out_widx,
+                             inputs=dnn_output_1,
+                             num_sampled=300,
+                             num_classes=vocab_size))
+              
+     	### [batch_size, num_steps-1, vocab_size]
+        self.predict_result = tf.reshape(prediction, [batch_size, num_steps-2, vocab_size] )
+        self.global_step = tf.Variable(0, trainable=False)
+        self.train_op = tf.train.AdamOptimizer().minimize(self.cost, global_step=self.global_step)
+        config = tf.ConfigProto(log_device_placement = True)
+        config.gpu_options.allow_growth = True
+        self.sess = tf.Session(config=config)
+
+    def train(self,input_sentences):
+        _,loss = self.sess.run([self.train_op,self.cost],feed_dict={self.sentences:input_sentences})
+        return loss
+
+    def predict(self,input_sentences):
+        return self.sess.run(self.predict_result,feed_dict={self.sentences:input_sentences})
     
     def initialize(self):
-        self.sess.run(tf.global_variables_initializer())                
+        self.sess.run(tf.global_variables_initializer()) 
+        
