1c6accfaa53daeb19326d733b8f7a679105a1243
RasonLiu <davidliu4417@gmail.com>
2017-05-09 23:19:16 +0800

    another wgan with tf.contrib.layers

--- hw3/gan_model.py
+++ hw3/gan_model.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import ops
+from functools import partial
 
 class GAN_model():
     def __init__(self, z_dim=100, batch_size=100, learning_rate=0.0002, img_shape=(96, 96, 3), optimizer_name='Adam'):
@@ -9,7 +10,7 @@ class GAN_model():
         self.learning_rate = learning_rate
         self.img_shape = img_shape
         self.pool_size = img_shape[0] // (2**4)
-        self.optimizer_name = optimizer_name
+        self.optimizer_name = optimizer
     
     def create_computing_graph(self):
         print("Setting up model...")
@@ -23,14 +24,14 @@ class GAN_model():
                                                                                  scope_name="discriminator",
                                                                                  reuse=False)
         
-        # reuse discriminator parameter when training generetor
+        # reuse discriminator parameter when training generator
         discr_fake_prob, logits_fake, feature_fake = self._discriminator(self.gen_images,
                                                                                  self.train_phase,
                                                                                  scope_name="discriminator",
                                                                                  reuse=True)
 
         self._discriminator_loss(logits_real, logits_fake)
-        self._generetor_loss(logits_fake, feature_fake, feature_real)
+        self._generator_loss(logits_fake, feature_fake, feature_real)
         
         train_variables = tf.trainable_variables()
         self.generator_variables = [v for v in train_variables if v.name.startswith("generator")]
@@ -166,7 +167,7 @@ class GAN_model():
         self.discriminator_loss = real_loss + fake_loss
         
     
-    def _generetor_loss(self, logits_fake, feature_fake, feature_real):
+    def _generator_loss(self, logits_fake, feature_fake, feature_real):
         gen_labels = tf.ones_like(logits_fake)
         gen_discr_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=gen_labels, logits=logits_fake))
         
@@ -227,12 +228,12 @@ class GAN_model():
         ops.save_imshow_grid(images, img_dir, "generated_%d.png" % self.global_steps, shape)
 
 class WGAN_model(GAN_model):
-    def __init__(self, z_dim=100, batch_size=100, learning_rate=0.0002, img_shape=(96, 96, 3), optimizer_name='RMSProp',
+    def __init__(self, z_dim=100, batch_size=100, learning_rate=0.0002, img_shape=(96, 96, 3), optimizer='RMSProp',
                        clip_value=(-0.01, 0.01), iter_ratio=5):
         self.clip_value = clip_value
         self.iter_ratio = iter_ratio
-        GAN_model.__init__(self, z_dim, batch_size, learning_rate, img_shape, optimizer_name)
-    
+        GAN_model.__init__(z_dim, batch_size, learning_rate, img_shape) 
+
     def _generator(self, z, train_phase, scope_name='generator'):
         shrink_size = self.pool_size
         with tf.variable_scope(scope_name) as scope:
@@ -338,12 +339,12 @@ class WGAN_model(GAN_model):
         self.discriminator_loss = tf.reduce_mean(logits_fake-logits_real)
         
     
-    def _generetor_loss(self, logits_fake, feature_fake, feature_real):
+    def _generator_loss(self, logits_fake, feature_fake, feature_real):
         self.generator_loss = tf.reduce_mean(-logits_fake)
         
     def train_model(self, dataLoader, max_epoch):
         self.global_steps = 0
-        clip_discriminator_var_op = [var.assign(tf.clip_by_value(var, self.clip_value[0], self.clip_value[1])) for
+        clip_discriminator_var_op = [var.assign(tf.clip_by_value(var, self.clip_values[0], self.clip_values[1])) for
                                          var in self.discriminator_variables]
 
         for i in range(max_epoch):
@@ -359,7 +360,7 @@ class WGAN_model(GAN_model):
                 else:
                     iteration = self.iter_ratio
 
-                for it_r in range(iteration):
+                for it_r in iteration:
                     self.sess.run(self.discriminator_train_op, feed_dict=feed_dict)
                     self.sess.run(clip_discriminator_var_op)
                     
@@ -375,4 +376,155 @@ class WGAN_model(GAN_model):
             if i % 4 == 0 and i != 0:
                 self.saver.save(self.sess, "model/wmodel_%d.ckpt" % self.global_steps, global_step=self.global_steps)
                 self._visualize_model('wresult')    
+
+class WGAN_model2(GAN_model):
+    def __init__(self, z_dim=100, batch_size=100, learning_rate=0.0002, img_shape=(96, 96, 3), optimizer='RMSProp',
+                       clip_value=(-0.01, 0.01), iter_ratio=5):
+        self.clip_value = clip_value
+        self.iter_ratio = iter_ratio
+        GAN_model.__init__(z_dim, batch_size, learning_rate, img_shape)
+    
+    def create_computing_graph(self):
+        print("Setting up model...")
+        
+        self._create_placeholder()
+        self.gen_images = self._generator(self.z_vec, self.train_phase, scope_name='generator')
+        
+        # re-initialize discriminator parameter when training discriminator
+        _, logits_real, _ = self._discriminator(self.real_imgs,
+                                                self.train_phase,
+                                                scope_name="discriminator",
+                                                reuse=False)
+        
+        # reuse discriminator parameter when training generator
+        _, logits_fake, _ = self._discriminator(self.gen_images,
+                                                self.train_phase,
+                                                scope_name="discriminator",
+                                                reuse=True)
+
+        self._discriminator_loss(logits_real, logits_fake)
+        self._generator_loss(logits_fake, None, None)
+        
+        
+        self.generator_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='generator')
+        self.discriminator_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='discriminator')
+        
+        counter_g = tf.Variable(trainable=False, initial_value=0, dtype=tf.int32)
+        counter_d = tf.Variable(trainable=False, initial_value=0, dtype=tf.int32)
+
+        if self.optimizer_name == 'Adam':
+            optimizer = tf.train.AdamOptimizer(self.learning_rate, beta1=0.5, beta2=0.9)
+        else:
+            optimizer = tf.train.RMSPropOptimizer(self.learning_rate, decay=0.9)
+
+        self.generator_train_op = tf.contrib.layers.optimize_loss(loss=self.generator_loss, learning_rate=self.learning_rate,
+                                                                  optimizer=partial(tf.train.AdamOptimizer, beta1=0.5, beta2=0.9) 
+                                                                                    if self.optimizer_name == 'Adam' 
+                                                                                    else tf.train.RMSPropOptimizer, 
+                                                                  variables=tf.generator_variables, 
+                                                                  global_step=counter_g)
+    
+        self.discriminator_train_op = tf.contrib.layers.optimize_loss(loss=self.discriminator_loss, learning_rate=learning_rate,
+                                                                      optimizer=partial(tf.train.AdamOptimizer, beta1=0.5, beta2=0.9) 
+                                                                                        if self.optimizer_name == 'Adam' 
+                                                                                        else tf.train.RMSPropOptimizer, 
+                                                                      variables=tf.discriminator_variables, 
+                                                                      global_step=counter_d)
+                                            
+        clipped_var_c = [tf.assign(var, tf.clip_by_value(var, self.clip_value[0], self.clip_value[1])) for var in tf.discriminator_variables]
+        # merge the clip operations on critic variables
+        with tf.control_dependencies([self.discriminator_train_op]):
+            self.discriminator_train_op = tf.tuple(clipped_var_c)
+
+    def _generator(self, z, train_phase, scope_name='generator'):
+        shrink_size = self.pool_size
+        with tf.variable_scope(scope_name) as scope:
+            train = tf.contrib.layers.fully_connected(z, shrink_size * shrink_size * 1024, 
+                                                      activation_fn=ops.leaky_relu, 
+                                                      normalizer_fn=tf.contrib.layersly.batch_norm)
+            train = tf.reshape(train, (-1, shrink_size, shrink_size, 1024))
+            train = tf.contrib.layers.conv2d_transpose(train, 512, 5, stride=2,
+                                        activation_fn=tf.nn.relu, 
+                                        normalizer_fn=tf.contrib.layers.batch_norm, padding='SAME', 
+                                        weights_initializer=tf.random_normal_initializer(0, 0.02),
+                                        normalizer_params={'is_training':self.train_phase})
+            train = tf.contrib.layers.conv2d_transpose(train, 256, 5, stride=2,
+                                        activation_fn=tf.nn.relu, 
+                                        normalizer_fn=tf.contrib.layers.batch_norm, padding='SAME', 
+                                        weights_initializer=tf.random_normal_initializer(0, 0.02), 
+                                        normalizer_params={'is_training':self.train_phase})
+            train = tf.contrib.layers.conv2d_transpose(train, 128, 5, stride=2,
+                                        activation_fn=tf.nn.relu, 
+                                        normalizer_fn=tf.contrib.layers.batch_norm, padding='SAME', 
+                                        weights_initializer=tf.random_normal_initializer(0, 0.02),
+                                        normalizer_params={'is_training':self.train_phase})
+            gen_img = tf.contrib.layers.conv2d_transpose(train, 3, 5, stride=2, 
+                                        activation_fn=tf.nn.tanh, 
+                                        normalizer_fn=tf.contrib.layers.batch_norm, padding='SAME', 
+                                        weights_initializer=tf.random_normal_initializer(0, 0.02),
+                                        normalizer_params={'is_training':self.train_phase})
+        return gen_img
             
+    def _discriminator(self, input_img, train_phase, scope_name='discriminator', reuse=False):
+        
+        with tf.variable_scope(scope_name) as scope:
+            if reuse:
+                scope.reuse_variables()
+            
+                img = tf.contrib.layers.conv2d(input_img, num_outputs=64, kernel_size=5, stride=2, 
+                                               activation_fn=ops.leaky_relu)
+                img = tf.contrib.layers.conv2d(img, num_outputs=128, kernel_size=5, stride=2, 
+                                               activation_fn=ops.leaky_relu, 
+                                               normalizer_fn=tf.contrib.layers.batch_norm,
+                                               normalizer_params={'is_training':self.train_phase})
+                img = tf.contrib.layers.conv2d(img, num_outputs=256, kernel_size=5, strides=2,
+                                               activation_fn=ops.leaky_relu, 
+                                               normalizer_fn=tf.contrib.layers.batch_norm,
+                                               normalizer_params={'is_training':self.train_phase})                            
+                img = tf.contrib.layersy.conv2d(img, num_outputs=512, kernel_size=5, strides=2,
+                                               activation_fn=ops.leaky_relu, 
+                                               normalizer_fn=tf.contrib.layers.batch_norm,
+                                               normalizer_params={'is_training':self.train_phase})
+                logit = tf.contrib.layers.fully_connected(tf.reshape(img, [self.batch_size, -1]), 1, activation_fn=None)
+                
+        return None, logit, None
+         
+         
+    def _discriminator_loss(self, logits_real, logits_fake):
+        self.discriminator_loss = tf.reduce_mean(logits_fake-logits_real)
+        
+    
+    def _generator_loss(self, logits_fake, feature_fake, feature_real):
+        self.generator_loss = tf.reduce_mean(-logits_fake)
+        
+    def train_model(self, dataLoader, max_epoch):
+        self.global_steps = 0
+
+        for i in range(max_epoch):
+            dataLoader.shuffle() 
+            batch_gen = dataLoader.batch_generator(batch_size=self.batch_size)
+
+            for batch_imgs in batch_gen:
+                batch_z = np.random.uniform(-1.0, 1.0, size=[self.batch_size, self.z_dim]).astype(np.float32)
+                feed_dict = {self.z_vec: batch_z, self.real_imgs: batch_imgs, self.train_phase: True}
+                
+                if self.global_steps < 25 or self.global_steps % 500 == 0:
+                    iteration = 40
+                else:
+                    iteration = self.iter_ratio
+
+                for it_r in iteration:
+                    self.sess.run(self.discriminator_train_op, feed_dict=feed_dict)
+                    
+                self.sess.run(self.generator_train_op, feed_dict=feed_dict)
+    
+                if self.global_steps % 20 == 0 and self.global_steps != 0:
+                    g_loss_val, d_loss_val = self.sess.run(
+                        [self.generator_loss, self.discriminator_loss], feed_dict=feed_dict)
+                    print("Epoch %d, Step: %d, generator loss: %g, discriminator_loss: %g" % (i, self.global_steps, g_loss_val, d_loss_val))
+                    
+                self.global_steps += 1
+            
+            if i % 4 == 0 and i != 0:
+                self.saver.save(self.sess, "model/wmodel_%d.ckpt" % self.global_steps, global_step=self.global_steps)
+                self._visualize_model('wresult')            
